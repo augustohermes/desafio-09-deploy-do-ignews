114fdc655dab1e992779a620ba9e362a8e95698e
Augusto Hermes <augustohermes@outlook.com>
2023-03-11 03:58:55 -0300

    Desafio 09 - RocketSeat
    Desafio 09 - RocketSeat

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,77 @@
+import { GetStaticProps } from "next";
+import Head from "next/head";
+import * as Prismic from "@prismicio/client";
+import { RichText } from "prismic-dom";
+import { getPrismicClient } from "../../services/prismic";
+import styles from "./styles.module.scss";
+import Link from "next/link";
+
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
+  return (
+    <>
+      <Head>
+        <title>Posts | Ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          {posts.map((post) => (
+            <Link href={`/posts/preview/${post.slug}`} key={post.slug}>
+              <a>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
+          ))}
+        </div>
+      </main>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.getAllByType("post", {
+    fetch: "[post.data.title, post.data.description]",
+    pageSize: 100,
+    predicates: [Prismic.predicates.at("document.type", "post")],
+  });
+
+  const posts = response.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.slices[0].primary.title),
+      excerpt:
+        post.data.slices[0].primary.description.find(
+          (content) => content.type === "paragraph"
+        )?.text ?? "",
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        "pt-BR",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+      ),
+    };
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
